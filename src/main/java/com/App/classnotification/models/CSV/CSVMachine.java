3aa58692166481c3f814fca46e8f1290b69a1ae1
ingfosbreak <ingfosbreak@outlook.com>
2023-02-23 19:08:10 +0700

    Update: prototype CSVMachine

--- src/main/java/com/App/classnotification/models/CSV/CSVMachine.java
+++ src/main/java/com/App/classnotification/models/CSV/CSVMachine.java
@@ -57,24 +57,34 @@ public class CSVMachine implements DataSourceCSV<ArrayList<ArrayList<String>>>{
         ArrayList<ArrayList<String>> stringStreams = new ArrayList<ArrayList<String>>();
 
         File file = new File(this.filePath);
-        FileReader reader = null;
-        BufferedReader buffer = null;
 
         try {
-            reader = new FileReader(file);
-            buffer = new BufferedReader(reader);
-
-
+           FileReader reader = new FileReader(file);
+           BufferedReader buffer = new BufferedReader(reader);
 
             String line = "";
+            int iteration = 0;
+
             while ( (line = buffer.readLine()) != null) {
+                if (iteration == 0){
+                    iteration++;
+                    continue;
+                }
+                if (line.isBlank()){
+                    continue;
+                }
                 ArrayList<String> innerStringStreams = new ArrayList<>();
                 for ( String word : line.split(",")){
-                    innerStringStreams.add(word);
-                    stringStreams.add(innerStringStreams);
+                    String realword = word.substring(1,word.length() - 1);
+                    innerStringStreams.add(realword);
+
                 }
+                stringStreams.add(innerStringStreams);
 
             }
+
+            buffer.close();
+            reader.close();
 
 
 
@@ -82,16 +92,7 @@ public class CSVMachine implements DataSourceCSV<ArrayList<ArrayList<String>>>{
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        } finally {
-
-            try {
-                buffer.close();
-                reader.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
         }
-
 
         return stringStreams;
     }
@@ -99,6 +100,42 @@ public class CSVMachine implements DataSourceCSV<ArrayList<ArrayList<String>>>{
 
     public void writeData( ArrayList<ArrayList<String>> stringStreams ){
 
+        File file = new File(this.filePath);
+
+        try {
+            FileWriter writer = new FileWriter(file,true);
+            BufferedWriter buffer = new BufferedWriter(writer);
+
+            for (ArrayList<String> innerArrays : stringStreams){
+
+                String line = "";
+
+                for (int i = 0; i < innerArrays.size(); i++){
+
+
+                    if ( i == 0 ){
+                        line = line + "\"" +innerArrays.get(i) + "\"";
+                    }
+                    else{
+                        line = line + "," + "\"" +innerArrays.get(i) + "\"";
+                    }
+
+
+                }
+
+                buffer.append(line);
+                buffer.newLine();
+
+            }
+
+            buffer.close();
+            writer.close();
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
@@ -106,16 +143,62 @@ public class CSVMachine implements DataSourceCSV<ArrayList<ArrayList<String>>>{
 
     public void writeHeader( String header ){
 
+
+        File file = new File(this.filePath);
+
+        try {
+            FileWriter writer = new FileWriter(file);
+            BufferedWriter buffer = new BufferedWriter(writer);
+
+            String line = header;
+
+            buffer.append(line);
+            buffer.newLine();
+            buffer.close();
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
     public void clearData(){
 
+        File file = new File(this.filePath);
+
+        try {
+            FileWriter writer = new FileWriter(file);
+            BufferedWriter buffer = new BufferedWriter(writer);
+
+            String line = "";
+
+            buffer.append(line);
+            buffer.close();
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public static void main(String[] args) {
 
         CSVMachine test = new CSVMachine("Data/ingfosbreak/info.csv");
+
+        System.out.println(test.readData());
+
+        ArrayList<ArrayList<String>> temp = test.readData();
+
+        test.clearData();
+
+        System.out.println(test.readData());
+
+        test.writeHeader("first,second,third");
+
+        test.writeData(temp);
 
         System.out.println(test.readData());
 
